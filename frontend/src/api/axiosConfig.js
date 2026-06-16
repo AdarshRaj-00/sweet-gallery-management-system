4d2cfdcd24b138822997f6cdfb58f928116de016
Adarsh Raj <adarshraj82945@gmail.com>
2026-06-16 12:00:24 +0530

    Update API baseURL to point to live Render backend

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080"
+  baseURL: "https://sweet-gallery-management-system.onrender.com"
 });
 
 api.interceptors.request.use((config) => {
